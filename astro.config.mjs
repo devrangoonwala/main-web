9e9e3073af148f08b969dd59768662064a1a3e79
Dev Rangoonwala <devcodes77@gmail.com>
2025-06-14 22:43:49 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://devrangoonwala.github.io',
+	base: 'my-repo'
+
 	integrations: [mdx(), sitemap()],
 });
